d4ccd9422b54de2fa6b5159f477f3b33562786c2
Gureet Sidhu <42849884+gs109111@users.noreply.github.com>
2022-09-20 22:12:01 -0600

    Add files via upload

---
+++ About_Me/static/js/script.js
@@ -0,0 +1,11 @@
+// Show Dialog Model 
+
+document.getElementById('resources').onclick = function() {
+    document.getElementById('resources_model').style.display = "inline-block";
+}
+
+// Hide Dialog Model
+
+document.getElementById('resources_model').onclick = function() {
+    document.getElementById('resources_model').style.display = 'none';
+}
